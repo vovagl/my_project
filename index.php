05a4b24862b9ac81c3f6792e0b9b8a76551c710a
VladimirG-1971 <vova16061971@gmail.com>
2026-04-16 19:09:27 +0300

    15.04

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $APPLICATION->IncludeComponent(
     "",
     array(
         "USE_CAPTCHA" => "N",
-        "EMAIL_TO" => "vova16061971@gmail.com",
+        "EMAIL_TO" => "test@mail.com",
         "REQUIRED_FIELDS" => array("NAME", "EMAIL", "MESSAGE"),
         "OK_TEXT" => "Сообщение отправлено",
     )
--- local/templates/my_site/components/bitrix/main.feedback/.default/template.php
+++ local/templates/my_site/components/bitrix/main.feedback/.default/template.php
@@ -1,57 +1,169 @@
 <?php
-session_start();
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Связаться");
+
+CModule::IncludeModule("iblock");
+$success = ($_GET["success"] ?? "") === "Y";
+
 if ($_SERVER["REQUEST_METHOD"] === "POST" && check_bitrix_sessid()) {
-    CModule::IncludeModule("iblock");
 
     $el = new CIBlockElement;
 
-    $el->Add([
+    $PROP = array(
+        "COMPANY" => $_POST["medicine_company"],
+        "EMAIL" => $_POST["medicine_email"],
+        "PHONE" => $_POST["medicine_phone"],
+        "MESSAGE" => $_POST["medicine_message"],
+    );
+
+    $arLoadProductArray = array(
         "IBLOCK_ID" => 3, 
-        "NAME" => $_POST["user_name"],
+        "NAME" => trim($_POST["medicine_name"]),
         "ACTIVE" => "Y",
-        "PROPERTY_VALUES" => [
-            "EMAIL" => $_POST["user_email"],
-            "MESSAGE" => $_POST["MESSAGE"]
-        ]
-    ]);
-    $_SESSION["FORM_SUCCESS"] = true;
-    LocalRedirect($APPLICATION->GetCurPage());
+        "PROPERTY_VALUES" => $PROP,
+    );
+    LocalRedirect($APPLICATION->GetCurPageParam("success=Y", ["success"]));
     die();
 }
-$success = false;
-
-if (!empty($_SESSION["FORM_SUCCESS"])) {
-    $success = true;
-    unset($_SESSION["FORM_SUCCESS"]);
-
-}
 ?>
-<?php if ($success): ?>
-    <div  id="success" style="color:green; font-weight:bold; text-align:center;">
-        ✔ Отправлено
+
+
+<div class="contact-form">
+    <div class="contact-form__head">
+        <div class="contact-form__head-title">Связаться</div>
+        <div class="contact-form__head-text">
+            Наши сотрудники помогут выполнить подбор услуги и расчет цены с учетом ваших требований
+        </div>
     </div>
-<?php endif; ?>
+      
+     <div  id="success" class=" <?= $success ? 'show' : '' ?>"> 
+        ✔ Отправлено
+    </div> 
+    <form novalidate class="contact-form__form" action="<?=POST_FORM_ACTION_URI?>" method="POST">
 
-<form method="post">
-    <?=bitrix_sessid_post()?>
-    <input type="text" name="user_name" placeholder="Имя">
-    <input type="email" name="user_email" placeholder="Email">
-    <textarea name="MESSAGE" placeholder="Сообщение"></textarea>
-    <button type="submit">Отправить</button>
-</form>
+        <?= bitrix_sessid_post() ?>
 
+        <div class="contact-form__form-inputs">
+
+            <div class="input contact-form__input">
+                <label class="input__label" for="medicine_name">
+                    <div class="input__label-text">Ваше имя*</div>
+                    <input class="input__input" type="text" id="medicine_name" name="medicine_name" value="" required="">
+                    <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+                </label>
+            </div>
+
+            <div class="input contact-form__input">
+                <label class="input__label" for="medicine_company">
+                    <div class="input__label-text">Компания/Должность*</div>
+                    <input class="input__input" type="text"  id="medicine_company" name="medicine_company" value="" required="">
+                    <div class="input__notification">Поле должно содержать не менее 3-х символов</div>
+                </label>
+            </div>
+
+            <div class="input contact-form__input">
+                <label class="input__label" for="medicine_email">
+                    <div class="input__label-text">Email*</div>
+                    <input class="input__input" type="email" id="medicine_email" name="medicine_email" value="" required="">
+                    <div class="input__notification">Неверный формат почты</div>
+                </label>
+            </div>
+
+            <div class="input contact-form__input">
+                <label class="input__label" for="medicine_phone">
+                    <div class="input__label-text">Номер телефона*</div>
+                    <input class="input__input" type="tel" id="medicine_phone" data-inputmask="'mask': '+79999999999', 'clearIncomplete': 'true'" maxlength="12"
+                    x-autocompletetype="phone-full" name="medicine_phone" value="" required="">
+                    <div class="input__notification">Правильно: +7XXXXXXXXXX</div>
+                </label>
+            </div>
+
+        </div>
+
+        <div class="contact-form__form-message">
+            <div class="input">
+                <label class="input__label" for="medicine_message">
+                    <div class="input__label-text">Сообщение</div>
+                    <textarea class="input__input" type="text" id="medicine_message" name="medicine_message" value=""></textarea>
+                    <div class="input__notification">Напишите сообщение</div>
+                </label>
+            </div>
+        </div>
+
+        <div class="contact-form__bottom">
+            <div class="contact-form__bottom-policy">
+                Нажимая «Отправить», вы подтверждаете, что ознакомлены,  полностью согласны и принимаете условия Согласия на обработку персональных данных.
+            </div>
+
+            <button class="form-button contact-form__bottom-button" data-success="Отправлено" data-error="Ошибка отправки" type="submit">
+                <div class="form-button__title">Отправить</div>
+            </button>
+        </div>
+
+    </form>
+</div>
 
 <script>
 document.addEventListener("DOMContentLoaded", function () {
     const msg = document.getElementById("success");
     if (msg) {
         setTimeout(() => {
-            msg.style.transition = "opacity 0.5s";
-            msg.style.opacity = "0";
-            setTimeout(() => {
-                msg.remove();
-            }, 500);
-        }, 4000);
-    }
+            msg.classList.remove("show");
+            }, 3000);
+        }
+     if (window.location.search.includes("success=Y")) {
+        const url = new URL(window.location);
+        url.searchParams.delete("success");
+        window.history.replaceState({}, document.title, url.pathname);
+    }        
 });
+</script>
+
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector(".contact-form__form");
+    const inputs = document.querySelectorAll(".input__input");
+
+    function validateField(input)  {
+        const wrapper = input.closest(".input");
+        let isValid = true;
+
+        if (input.value.trim().length < 3) {
+            isValid = false;
+        }
+         if 
+            (input.id === "medicine_message") 
+        {
+           isValid = input.value.trim().length >= 1;
+        }
+        if (input.type === "email") {
+            const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailPattern.test(input.value.trim())) {
+                isValid = false;
+            }
+        }
+        if (input.type === "tel") {
+            const phone = input.value.replace(/\D/g, "");
+            if (phone.length < 11) {
+                isValid = false;
+            }
+        }
+          wrapper.classList.toggle("error", !isValid);
+           return isValid;
+        }
+       form.addEventListener("submit", function (e) {
+
+        let allValid = true;
+
+        inputs.forEach(input => {
+            const ok = validateField(input);
+            if (!ok) allValid = false;
+        });
+
+        if (!allValid) {
+            e.preventDefault(); 
+        }
+    });
+    })
 </script>
